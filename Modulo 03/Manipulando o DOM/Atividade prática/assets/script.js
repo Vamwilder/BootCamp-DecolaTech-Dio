ffd9f3911270249e1d6e67a3120a24db25b9c3b5
Vamwilder <kiamdemouralechinovski@hotmail.com>
2022-06-17 20:02:45 -0300

    assets

--- Modulo 03/Manipulando o DOM/Atividade prática/assets/script.js
+++ Modulo 03/Manipulando o DOM/Atividade prática/assets/script.js
@@ -1,9 +1,27 @@
-const text = document.getElementById("txt");
+const text  = document.getElementById("txt");
 const botao = document.getElementById("btn");
+const body  = document.getElementById("body");
 
 botao.addEventListener("click", darkMode);
 
 function darkMode(){
-    text.innerText = "Dark Mode ON";
-    botao.innerText = "Light Mode";
+    if (text.textContent === "Light Mode ON") {
+        // html
+        text.innerText  = "Dark Mode ON";
+        botao.innerText = "Light Mode";
+        // CSS
+        body.style.backgroundColor  = "black";
+        text.style.color            = "white";
+        botao.style.backgroundColor = "black";
+        botao.style.color           = "white";
+    } else {
+        // html
+        text.innerText  = "Light Mode ON";
+        botao.innerText = "Dark Mode";
+        // CSS
+        body.style.backgroundColor  = "white";
+        text.style.color            = "black";   
+        botao.style.backgroundColor = "white";
+        botao.style.color           = "black";     
+    }    
 }
